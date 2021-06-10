e3c2954d5c227fe12cbd182a080689438b6f50c2
Pascal Fong Kye <pascal.fongkye@manomano.com>
2021-06-10 14:52:38 +0200

    update: img profile and titles

--- components/layout.js
+++ components/layout.js
@@ -4,13 +4,13 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Pascal';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Pascal Blog';
 
 export default function Layout({ children, home }) {
     return <div className={styles.container}>
         <Head>
             <link rel="icon" href="/favicon.ico" />
-            <meta name="description" content="Learn how to build a personal website using Next.js" />
+            <meta name="description" content="Blog about tech stuff" />
             <meta property="og:image" content={`https://og-image.now.sh/${encodeURI(siteTitle
             )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
             <meta name="og:title" content={siteTitle} />
@@ -19,9 +19,10 @@ export default function Layout({ children, home }) {
         <header className={styles.header}>
             {home ? (
                 <>
-                    <img src="/images/profile.jpg"
+                    <img src="/images/pfk_profile.png"
                         className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                        alt={name} />
+                        alt={name} 
+                        width={2482} height={3508}/>
                     <h1 className={utilStyles.heading2X1}>{name}</h1>
                 </>
             ) : (
@@ -29,7 +30,7 @@ export default function Layout({ children, home }) {
                         <Link href="/">
                             <a>
                                 <img
-                                    src="/images/profile.jpg"
+                                    src="/images/pfk_profile.png"
                                     className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                                     alt={name} />
                             </a>

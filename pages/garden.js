af9d99841e3da033e6135b4152666234099fea27
Pascal Fong Kye <pfongkye@yahoo.com>
2022-09-25 15:49:15 +0000

    feat: add menu

---
+++ pages/garden.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+
+export default function Garden(){
+    return <Layout>
+        <Head>
+            <title>Digital Garden</title>
+        </Head>
+        <div>Garden</div>
+    </Layout>
+}

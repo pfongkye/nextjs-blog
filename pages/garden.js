f0d7ec6616684adb066e67908069b57b09851ed8
Pascal Fong Kye <pfongkye@yahoo.com>
2022-09-25 16:19:51 +0000

    feat: add resources for digital garden

--- pages/garden.js
+++ pages/garden.js
@@ -1,11 +1,21 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout from "../components/layout";
 
-export default function Garden(){
+import { learnings } from "../garden/resources.json";
+
+export default function Garden() {
     return <Layout>
         <Head>
             <title>Digital Garden</title>
         </Head>
-        <div>Garden</div>
+        <ul>
+            {learnings.map(({ title, link }, i) => (
+                <li key={`learning${i}`}>
+                    <Link href={link}>
+                        <a target='_blank' rel='noreferrer'>{title}</a>
+                    </Link>
+                </li>))}
+        </ul>
     </Layout>
 }
